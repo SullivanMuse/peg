30321284e4c2cbb8075f3d345472dee1a247292e
Sullivan Muse <sullivan.muse@gmail.com>
2020-12-28 09:38:46 -0600

    Implement left recursion checker

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ use syn::{
     parse_macro_input,
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum Atom<Key> {
     Ident(Key),
     Str(LitStr),
@@ -61,7 +61,7 @@ impl Parse for Atom<Ident> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum Expr<Key> {
     Alt(Box<Self>, Box<Self>),
     Cat(Vec<Self>),
@@ -270,10 +270,148 @@ struct Compiler {
     indices: HashMap<usize, Ident>,
 }
 
+/// A structure that treats references as equal
+#[derive(Debug, Eq)]
+struct RefEq<'a, T>(&'a T);
+
+impl<'a, T> std::hash::Hash for RefEq<'a, T> {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: std::hash::Hasher,
+    {
+        (self.0 as *const T).hash(state)
+    }
+}
+
+impl<'a, 'b, T> PartialEq<RefEq<'b, T>> for RefEq<'a, T> {
+    fn eq(&self, other: &RefEq<'b, T>) -> bool {
+        self.0 as *const T == other.0 as *const T
+    }
+}
+
+/// Determine if a rule is left recursive
+fn left_rec(rules: &Vec<Rule<usize>>, rule: usize) -> bool {
+    struct Checker<'a> {
+        // Basic info
+        rules: &'a Vec<Rule<usize>>,
+        rule: usize,
+
+        // Memos
+        transparent_map: HashMap<RefEq<'a, Expr<usize>>, bool>,
+        left_rec_expr_map: HashMap<RefEq<'a, Expr<usize>>, bool>,
+    }
+
+    impl<'a> Checker<'a> {
+        /// Returns whether or not a rule can consume no input
+        fn transparent(&mut self, expr: &'a Expr<usize>) -> bool {
+            let ref_eq = RefEq(expr);
+            if let Some(result) = self.left_rec_expr_map.get(&ref_eq) {
+                return *result
+            }
+            let result = match expr {
+                // Alternative may consume no input if either alternative may consume no input
+                Expr::Alt(left, right)
+                    => self.transparent(left)
+                    || self.transparent(right),
+                
+                // Concatenation may consume no input if all rules may consume no input
+                Expr::Cat(inner) => inner.iter().all(|inner| self.transparent(inner)),
+                
+                // The following may consume no input if the inner parser may consume no input
+                Expr::Many1(inner) => self.transparent(inner),
+                Expr::Atomic(inner) => self.transparent(inner),
+                Expr::Named(_, inner) => self.transparent(inner),
+                
+                // The following may consume no input unconditionally
+                Expr::Many0(_)
+                | Expr::Optional(_)
+                | Expr::Pos(_)
+                | Expr::Neg(_) => true,
+
+                // Atoms must be refined
+                Expr::Atom(atom) => match atom {
+                    // A call to another rule is transparent if the rule is transparent
+                    Atom::Ident(key) => {
+                        let inner = &self.rules.get(*key).unwrap().expr;
+                        self.transparent(inner)
+                    }
+
+                    // Parenthesized atoms are the result of the inner expr
+                    Atom::Paren(inner) => self.transparent(inner),
+
+                    // Literals must consume input
+                    _ => false,
+                }
+            };
+            self.left_rec_expr_map.insert(ref_eq, result);
+            result
+        }
+
+        fn left_rec_expr(&mut self, expr: &'a Expr<usize>) -> bool {
+            let ref_eq = RefEq(expr);
+            if let Some(result) = self.left_rec_expr_map.get(&ref_eq) {
+                return *result
+            }
+            let result = match expr {
+                // An alternative is left recursive if one of the alternatives is left recursive
+                Expr::Alt(left, right)
+                    => self.left_rec_expr(left)
+                    || self.left_rec_expr(right),
+                
+                // A concatenation is left recursive if any of the possible starts of the concatenation are left recursive
+                Expr::Cat(inner) => {
+                    for inner in inner {
+                        if self.left_rec_expr(inner) {
+                            return true
+                        } else if !self.transparent(inner) {
+                            break
+                        }
+                    }
+                    false
+                }
+
+                // Left recursive if the inner is
+                Expr::Many1(inner)
+                | Expr::Optional(inner)
+                | Expr::Pos(inner)
+                | Expr::Neg(inner)
+                | Expr::Atomic(inner) // TODO: Fix for implicit spaces
+                | Expr::Many0(inner)
+                | Expr::Named(_, inner) => self.left_rec_expr(inner),
+
+                // Atoms MAY be left recursive
+                Expr::Atom(atom) => match atom {
+                    // A call dereferences to determine if left recursive
+                    Atom::Ident(key) => {
+                        if *key == self.rule {
+                            true
+                        } else {
+                            let inner = &self.rules.get(*key).unwrap().expr;
+                            self.left_rec_expr(inner)
+                        }
+                    }
+
+                    // Simple
+                    Atom::Paren(inner) => self.left_rec_expr(inner),
+
+                    // Literals can't be left recursive because they don't contain other rules.
+                    _ => false
+                }
+            };
+            self.left_rec_expr_map.insert(ref_eq, result);
+            result
+        }
+    }
+
+    let expr = &rules.get(rule).unwrap().expr;
+    Checker { rules, rule, transparent_map: HashMap::new(), left_rec_expr_map: HashMap::new() }.left_rec_expr(expr)
+}
+
 #[proc_macro]
 pub fn peg(input: TokenStream) -> TokenStream {
     let grammar = parse_macro_input!(input as Grammar);
     let compiler = grammar.to_compiler();
+    dbg!(left_rec(&compiler.rules, 0));
     dbg!(compiler);
     quote!().into()
 }
